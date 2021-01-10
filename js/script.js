be019d5803a763a203d2750b28c37277ae59ebb1
Chng Boon Chong <S10208460@connect.np.edu.sg>
2021-01-10 16:20:50 +0800

    API call switched to https

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-$.getJSON("http://api.openweathermap.org/data/2.5/weather?q=singapore&units=metric&appid=73699de2b84a44f184293ea613935bc8", function(data)
+$.getJSON("https://api.openweathermap.org/data/2.5/weather?q=singapore&units=metric&appid=73699de2b84a44f184293ea613935bc8", function(data)
 {
     console.log(data);
 
