38b08b95e63068f170bcedd7739bafb05895c2ae
Chng Boon Chong <S10208460@connect.np.edu.sg>
2021-01-10 21:03:41 +0800

    Added messages and pitch

--- js/script.js
+++ js/script.js
@@ -1,16 +1,21 @@
+// Call API
 $.getJSON("https://api.openweathermap.org/data/2.5/weather?q=singapore&units=metric&appid=73699de2b84a44f184293ea613935bc8", function(data)
 {
-    console.log(data);
+    //console.log(data);
 
+    // Take icon image and place into html
     var icon = "https://openweathermap.org/img/w/" + data.weather[0].icon + ".png";
     $('.icon').attr('src', icon);
 
+    // Take temperature data and put into html
     var temp = data.main.temp  + "°";
     $('.temp').append(temp);
 
+    // Take weather forecast data and put into html
     var forecast = data.weather[0].main;
     $('.forecast').append(forecast);
 
+    // Get current date and put into html
     currentDate = new Date();
     year = currentDate.getFullYear();
     month = currentDate.getMonth() + 1;
